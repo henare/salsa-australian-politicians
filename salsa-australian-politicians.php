d882d20edfdcc7268849777857b907f87f04c699
Henare Degan <henare.degan@gmail.com>
2011-05-12 16:44:02 +1000

    Correct the license header

--- salsa-australian-politicians.php
+++ salsa-australian-politicians.php
@@ -6,22 +6,22 @@ Description: Allows your site users to send emails to Australian Politicians via
 Version: 0.0.1
 Author: Henare Degan
 Author URI: http://www.henaredegan.com/
-License: GPL v3
+License: AGPL v3
 */
 
 /*  Copyright (C) 2011  Henare Degan
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
