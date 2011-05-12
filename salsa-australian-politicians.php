b0e4419f5c0e6206255e0c36c1d2f3dbc037ec81
Henare Degan <henare.degan@gmail.com>
2011-05-12 10:43:46 +1000

    Add find state by postcode function

--- salsa-australian-politicians.php
+++ salsa-australian-politicians.php
@@ -233,7 +233,7 @@ function salsa_campaigns_select_mp_page($postcode) {
   }
 
   // Get Senators from the OpenAustralia API
-  $state = findState($postcode);
+  $state = salsa_campaigns_find_state($postcode);
   $senators = simplexml_load_file('http://www.openaustralia.org/api/getSenators?output=xml&key=' . $salsa_campaigns_options['oa_api_key'] . '&state=' . $state);
 
   foreach ( $senators->match as $senator ) {
@@ -419,4 +419,82 @@ function salsa_campaigns_salsa_logon() {
     $salsa_campaigns_options['salsa_username'],
     $salsa_campaigns_options['salsa_password']
   );
+}
+
+/**
+* Returns the state for a postcode.
+* eg. NSW
+*
+* @author http://waww.com.au/ramblings/determine-state-from-postcode-in-australia
+* @link http://en.wikipedia.org/wiki/Postcodes_in_Australia#States_and_territories
+*/
+function salsa_campaigns_find_state($postcode) {
+  $ranges = array(
+    'NSW' => array(
+      1000, 1999,
+      2000, 2599,
+      2619, 2898,
+      2921, 2999
+    ),
+    'ACT' => array(
+      200, 299,
+      2600, 2618,
+      2900, 2920
+    ),
+    'VIC' => array(
+      3000, 3999,
+      8000, 8999
+    ),
+    'Queensland' => array(
+      4000, 4999,
+      9000, 9999
+    ),
+    'SA' => array(
+      5000, 5999
+    ),
+    'WA' => array(
+      6000, 6797,
+      6800, 6999
+    ),
+    'TAS' => array(
+      7000, 7999
+    ),
+    'NT' => array(
+      800, 999
+    )
+  );
+  $exceptions = array(
+    872 => 'NT',
+    2540 => 'NSW',
+    2611 => 'ACT',
+    2620 => 'NSW',
+    3500 => 'VIC',
+    3585 => 'VIC',
+    3586 => 'VIC',
+    3644 => 'VIC',
+    3707 => 'VIC',
+    2899 => 'NSW',
+    6798 => 'WA',
+    6799 => 'WA',
+    7151 => 'TAS'
+  );
+
+  $postcode = intval($postcode);
+  if ( array_key_exists($postcode, $exceptions) ) {
+    return $exceptions[$postcode];
+  }
+
+  foreach ($ranges as $state => $range)
+  {
+    $c = count($range);
+    for ($i = 0; $i < $c; $i+=2) {
+      $min = $range[$i];
+      $max = $range[$i+1];
+      if ( $postcode >= $min && $postcode <= $max ) {
+        return $state;
+      }
+    }
+  }
+
+  return null;
 }
